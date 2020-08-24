aa8b42c44110651c5bd439a7bd75f5fde31667e6
Chris Tralie <chris.tralie@gmail.com>
2020-08-24 18:31:52 -0400

    Revert "Merge pull request #104 from ulupo/coo_format_patch"
    This reverts commit 07adc60c518dbdd59e443c3bd82f1ce8452a5c98.

--- ripser/ripser.py
+++ ripser/ripser.py
@@ -299,24 +299,16 @@ def ripser(
         dm = sparse.coo_matrix(dm)
 
     if sparse.issparse(dm):
-        if sparse.isspmatrix_coo(dm):
-            # If the matrix is already COO, we need to order the row and column indices
-            # lexicographically to avoid errors. See issue #103
-            row, col, data = dm.row, dm.col, dm.data
-            lex_sort_idx = np.lexsort((col, row))
-            row, col, data = row[lex_sort_idx], col[lex_sort_idx], data[lex_sort_idx]
-        else:
-            # Lexicographic ordering is performed by scipy upon conversion to COO
-            coo = dm.tocoo()
-            row, col, data = coo.row, coo.col, coo.data
+        coo = dm.tocoo()
         res = DRFDMSparse(
-            row.astype(dtype=np.int32, order="C"),
-            col.astype(dtype=np.int32, order="C"),
-            np.array(data, dtype=np.float32, order="C"),
+            coo.row.astype(dtype=np.int32, order="C"),
+            coo.col.astype(dtype=np.int32, order="C"),
+            np.array(coo.data, dtype=np.float32, order="C"),
             n_points,
             maxdim,
             thresh,
             coeff,
+            do_cocycles,
         )
     else:
         I, J = np.meshgrid(np.arange(n_points), np.arange(n_points))
