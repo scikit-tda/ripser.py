b68794a57cc1d74905071ceaea9308ee3b83ac5d
Michael Catanzaro <catanzaromj@pm.me>
2024-02-26 20:46:07 -0500

    Add dummy package name for testing

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ except:
 
 ## Get version information from _version.py
 import re
-VERSIONFILE="ripser/_version.py"
+
+VERSIONFILE = "ripser/_version.py"
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
@@ -25,63 +26,58 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-# Use README.md as the package long description  
-with open('README.md') as f:
+# Use README.md as the package long description
+with open("README.md") as f:
     long_description = f.read()
 
+
 class CustomBuildExtCommand(build_ext):
-    """ This extension command lets us not require numpy be installed before running pip install ripser 
-        build_ext command for use when numpy headers are needed.
+    """This extension command lets us not require numpy be installed before running pip install ripser
+    build_ext command for use when numpy headers are needed.
     """
 
     def run(self):
         # Import numpy here, only when headers are needed
         import numpy
+
         # Add numpy headers to include_dirs
         self.include_dirs.append(numpy.get_include())
         # Call original build_ext command
         build_ext.run(self)
 
+
 extra_compile_args = ["-Ofast", "-D_hypot=hypot"]
 extra_link_args = []
 
 if platform.system() == "Windows":
-    extra_compile_args.extend([
-        # Supported by Visual C++ >=14.1
-        '/std:c++14'
-    ])
+    extra_compile_args.extend(
+        [
+            # Supported by Visual C++ >=14.1
+            "/std:c++14"
+        ]
+    )
 elif platform.system() == "Darwin":
-    extra_compile_args.extend([
-        '-std=c++11', 
-        "-mmacosx-version-min=10.9"
-    ])
-    extra_link_args.extend([
-        "-stdlib=libc++", 
-        "-mmacosx-version-min=10.9"
-    ])
+    extra_compile_args.extend(["-std=c++11", "-mmacosx-version-min=10.9"])
+    extra_link_args.extend(["-stdlib=libc++", "-mmacosx-version-min=10.9"])
 else:
-    extra_compile_args.extend([
-        "-std=c++11"
-    ])
+    extra_compile_args.extend(["-std=c++11"])
 
-macros = [
-    ("USE_COEFFICIENTS", 1),
-    ("NDEBUG", 1),
-    ("ASSEMBLE_REDUCTION_MATRIX", 1)
-]
+macros = [("USE_COEFFICIENTS", 1), ("NDEBUG", 1), ("ASSEMBLE_REDUCTION_MATRIX", 1)]
 
 # Robinhood
-robinhood_path = os.path.join('ripser', 'robinhood')
+robinhood_path = os.path.join("ripser", "robinhood")
 if os.path.isdir(robinhood_path):
     macros.extend([("USE_ROBINHOOD_HASHMAP", 1)])
 
-    robinhood_include_path = os.path.join('src', 'include')
+    robinhood_include_path = os.path.join("src", "include")
     if platform.system() == "Windows":
-        extra_compile_args.extend(['/I'+os.path.join(robinhood_path,
-                                                     robinhood_include_path)])
+        extra_compile_args.extend(
+            ["/I" + os.path.join(robinhood_path, robinhood_include_path)]
+        )
     else:
-        extra_compile_args.extend(['-I'+os.path.join(robinhood_path,
-                                                     robinhood_include_path)])
+        extra_compile_args.extend(
+            ["-I" + os.path.join(robinhood_path, robinhood_include_path)]
+        )
 
 ext_modules = Extension(
     "pyRipser",
@@ -89,12 +85,12 @@ ext_modules = Extension(
     define_macros=macros,
     extra_compile_args=extra_compile_args,
     extra_link_args=extra_link_args,
-    language="c++"
+    language="c++",
 )
 
 
 setup(
-    name="ripser",
+    name="ripser_test_1934",
     version=verstr,
     description="A Lean Persistent Homology Library for Python",
     long_description=long_description,
@@ -102,43 +98,32 @@ setup(
     author="Chris Tralie, Nathaniel Saul",
     author_email="chris.tralie@gmail.com, nat@riverasaul.com",
     url="https://ripser.scikit-tda.org",
-    license='MIT',
-    packages=['ripser'],
+    license="MIT",
+    packages=["ripser"],
     ext_modules=cythonize(ext_modules),
-    install_requires=[
-        'Cython',
-        'numpy',
-        'scipy',
-        'scikit-learn',
-        'persim'
-    ],
+    install_requires=["Cython", "numpy", "scipy", "scikit-learn", "persim"],
     extras_require={
-        'testing': [ # `pip install -e ".[testing]"``
-            'pytest'  
+        "testing": [  # `pip install -e ".[testing]"``
+            "pytest"
         ],
-        'docs': [ # `pip install -e ".[docs]"`
-            'sktda_docs_config'
+        "docs": [  # `pip install -e ".[docs]"`
+            "sktda_docs_config"
         ],
-        'examples': [
-            'persim',
-            'tadasets',
-            'jupyter',
-            'pillow'
-        ]
+        "examples": ["persim", "tadasets", "jupyter", "pillow"],
     },
-    cmdclass={'build_ext': CustomBuildExtCommand},
-    python_requires='>=3.6',
+    cmdclass={"build_ext": CustomBuildExtCommand},
+    python_requires=">=3.6",
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Education',
-        'Intended Audience :: Financial and Insurance Industry',
-        'Intended Audience :: Healthcare Industry',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Healthcare Industry",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='persistent homology, rips filtration, persistence diagrams, topology data analysis, algebraic topology, unsupervised learning'
+    keywords="persistent homology, rips filtration, persistence diagrams, topology data analysis, algebraic topology, unsupervised learning",
 )
