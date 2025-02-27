fae2dedc5b1606fb40df986fe621e9d1b7662736
Michael Catanzaro <catanzaromj@pm.me>
2025-02-27 12:15:43 -0500

    Add html_context to globals

--- docs/conf.py
+++ docs/conf.py
@@ -27,4 +27,6 @@ html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
 
 # Tell Jinja2 templates the build is running on Read the Docs
 if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
     html_context["READTHEDOCS"] = True
